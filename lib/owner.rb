58eeeef324eabe93b18db0d1b2bfa06324594831
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-04 04:18:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  @@all=[ ]
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name= name
+    @@all<< self
+  end
+  
+  def species
+    @species= "human"
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+end
+
+def cats 
+  Cat.all {|cats|==self}
+  
+end
+end
+
+
 end
