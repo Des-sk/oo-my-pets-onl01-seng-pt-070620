18c76f6fab1a334595bbbbfef3323200c2849997
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-04 04:31:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,8 +28,12 @@ class Owner
   end
 
   def cats 
-  Cat.all.select{|cat| cat.owner == self}
+    Cat.all.select{|cat| cat.owner == self}
   end
+  
+  def dogs 
+    Dog.all.select{|dog| dog.owner == self}
+    
 
 
 end
